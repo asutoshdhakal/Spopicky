e345730bc969906f9b0d0d092bd36a9c2a87c55b
Asu Dhakal <dhakalasutosh@gmail.com>
2017-04-17 21:30:44 -0500

    Working with image page

--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 # views.py
-
+from flask import request
 from flask import render_template
-
 from app import app
+import spotipy as sp
 
+#Main page of the app.
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/', methods=['POST'])
+def url_post():
+    imageURL = request.form['text']
+    return render_template("image.html", imageURL=imageURL)
